042f9d7621b0ed3ebfaf865b4b089a4c6d8bc86c
Maclemon17 <maclemon17@gmail.com>
2023-08-02 15:39:53 +0100

    feat: upload-product-page and 404 page

---
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,93 @@
+import { Button, Input, Select, Typography } from 'antd';
+import { Form } from 'formik';
+import React, { useState } from 'react'
+
+const Continents = [
+    { key: 1, value: "Africa" },
+    { key: 2, value: "Europe" },
+    { key: 3, value: "Asia" },
+    { key: 4, value: "North America" },
+    { key: 5, value: "Souuth America" },
+    { key: 6, value: "Australia" },
+    { key: 7, value: "Antartica" },
+
+];
+
+const { Title } = Typography;
+const { TextArea } = Input;
+
+const UploadProductPage = () => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState(0);
+    const [continent, setContinent] = useState(1);
+
+    const onTitleChange = (e) => {
+        setTitle(e.currentTarget.value);
+    }
+    const onDescriptionChange = (e) => {
+        setDescription(e.currentTarget.value);
+    }
+    const onPriceChange = (e) => {
+        setPrice(e.currentTarget.value);
+    }
+    const onContinentChange = (e) => {
+        console.log(e.currentTarget.value);
+        setContinent(e.currentTarget.value);
+    }
+
+    return (
+        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+            <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+                <Title level={2}>Upoad Travel Product</Title>
+            </div>
+
+            <Form onSubmit={"k"}>
+
+                {/* DROPZONE */}
+
+                <br />
+                <br />
+                <div className="form-group">
+                    <label level={2} htmlFor="title">Title</label>
+                    <Input type="text" name="title" id=""
+                        onChange={onTitleChange}
+                        value={title}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="desc">Description</label>
+                    <TextArea type="text" name="desc" id=""
+                        onChange={onDescriptionChange}
+                        value={description}
+                    ></TextArea>
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="price">Price($)</label>
+                    <Input type="number" name="price" id=""
+                        onChange={onPriceChange}
+                        value={price}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="continents">Continent</label>
+                    <Select name="continents" id="" onChange={onContinentChange}>
+                        {
+                            Continents.map(continent => (
+                                <Select.Option key={continent.key} value={continent.key}>{continent.value}</Select.Option>
+                            ))
+                        }
+                    </Select>
+                </div>
+                <br />
+                <Button>Submit</Button>
+                {/* <button>Submit</button> */}
+            </Form>
+        </div>
+    )
+}
+
+export default UploadProductPage
